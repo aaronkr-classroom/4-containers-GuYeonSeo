3e6cf96a332f672943771bc95af53018acde00a2
GuYeonSeo <rnrn9204@naver.com>
2025-03-28 17:12:20 +0900

    week4

---
+++ week4/read_words.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+//read_words.h
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::istream& read_words(std::istream&, std::vector<std::string>&);
+
+#endif
